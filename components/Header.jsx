28d4eb9bd08744f0232f7be50cb5853a9edce80b
Adilet Nasirov <adilet.nasirov@yahoo.com>
2022-09-09 22:25:36 -0700

    header component

---
+++ components/Header.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header>
+      {/* Left div */}
+      <div>
+        <Image></Image>
+      </div>
+      {/* Middle one */}
+      <div>Center</div>
+      {/* Rigth div */}
+      <div>Men</div>
+    </header>
+  );
+};
+
+export default Header;
