a948a5792ee0bb5430b886eae3857a0587d86911
Adilet Nasirov <adilet.nasirov@yahoo.com>
2022-09-11 08:43:49 -0700

    navbar icon and search bar

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,16 +1,39 @@
 import React from "react";
-
+import Image from "next/image";
+import { SearchIcon } from "@heroicons/react";
 const Header = () => {
   return (
-    <header>
+    <header className="sticky top-0 z-50 grid grid-cols-3 bg-white shadow-md p-3 md:px-10 ">
       {/* Left div */}
-      <div>
-        <Image></Image>
+      <div className="relative flex items-center h-10 cursor-pointer my-auto">
+        <Image
+          src="https://links.papareact.com/qd3"
+          layout="fill"
+          objectFit="contain"
+          objectPosition="left"
+        />
       </div>
       {/* Middle one */}
-      <div>Center</div>
-      {/* Rigth div */}
-      <div>Men</div>
+      <div>
+        <input type="text" placeholder="Start your search" />
+        <SearchIcon />
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={1.5}
+          stroke="currentColor"
+          className="w-6 h-6"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z"
+          />
+        </svg>
+      </div>
+      {/* Right div */}
+      <div></div>
     </header>
   );
 };
