9f36284440f62f87e9dc65751e70dcc09e99f3d0
Adilet Nasirov <adilet.nasirov@yahoo.com>
2022-09-12 11:08:07 -0700

    search input fiel has finished

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Image from "next/image";
-import { SearchIcon } from "@heroicons/react";
+import { SearchIcon } from "@heroicons/react/solid";
 const Header = () => {
   return (
     <header className="sticky top-0 z-50 grid grid-cols-3 bg-white shadow-md p-3 md:px-10 ">
@@ -14,23 +14,13 @@ const Header = () => {
         />
       </div>
       {/* Middle one */}
-      <div>
-        <input type="text" placeholder="Start your search" />
-        <SearchIcon />
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          strokeWidth={1.5}
-          stroke="currentColor"
-          className="w-6 h-6"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z"
-          />
-        </svg>
+      <div className="flex items-center md:border-2 rounded-full py-2 md:shadow-sm">
+        <input
+          className="pl-5 bg-transparent outline-none flex-grow text-gray-600 placeholder-gray-400"
+          type="text"
+          placeholder="Start your search"
+        />
+        <SearchIcon className="hidden md:inline-flex h-8 bg-red-400 text-white rounded-full p-2 cursor-pointer md: mx-2 " />
       </div>
       {/* Right div */}
       <div></div>
