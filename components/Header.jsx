8d89bfb18b09fb7d0511e4e0ee38c96e74a09988
Adilet Nasirov <adilet.nasirov@yahoo.com>
2022-09-12 11:18:49 -0700

    navbar right side finished

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,6 +1,12 @@
 import React from "react";
 import Image from "next/image";
-import { SearchIcon } from "@heroicons/react/solid";
+import {
+  SearchIcon,
+  GlobeAltIcon,
+  MenuIcon,
+  UserCircleIcon,
+  UsersIcon,
+} from "@heroicons/react/solid";
 const Header = () => {
   return (
     <header className="sticky top-0 z-50 grid grid-cols-3 bg-white shadow-md p-3 md:px-10 ">
@@ -23,7 +29,14 @@ const Header = () => {
         <SearchIcon className="hidden md:inline-flex h-8 bg-red-400 text-white rounded-full p-2 cursor-pointer md: mx-2 " />
       </div>
       {/* Right div */}
-      <div></div>
+      <div className="flex  space-x-4 items-center justify-end text-gray-500">
+        <p className="cursor-pointer hidden md:inline">Become a host</p>
+        <GlobeAltIcon className="h-6 " />
+        <div className="flex items-center space-x-2 border-2 p-2 rounded-full">
+          <MenuIcon className="h-6 cursor-pointer" />
+          <UserCircleIcon className="h-6 cursor-pointer" />
+        </div>
+      </div>
     </header>
   );
 };
