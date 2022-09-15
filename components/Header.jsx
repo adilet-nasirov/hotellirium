9c874cc7ff6c7e2d2a234e3e0915fe6e9da5f20b
Adilet Nasirov <adilet.nasirov@yahoo.com>
2022-09-15 12:12:12 -0700

    searching with dates popup window

--- components/Header.jsx
+++ components/Header.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import Image from "next/image";
 import "react-date-range/dist/styles.css"; // main style file
 import "react-date-range/dist/theme/default.css"; // theme css file
+import { DateRangePicker } from "react-date-range";
 import {
   SearchIcon,
   GlobeAltIcon,
@@ -11,6 +12,22 @@ import {
 } from "@heroicons/react/solid";
 const Header = () => {
   const [searchInput, setSearchInput] = useState("");
+  const [startDate, setStartDate] = useState(new Date());
+  const [endDate, setEndDate] = useState(new Date());
+  const [nofGuests, setNofGuests] = useState(1);
+  const selectionRange = {
+    startDate: startDate,
+    endDate: endDate,
+    key: "Selection",
+  };
+  const handleSelect = (ranges) => {
+    setStartDate(ranges.Selection.startDate);
+    setEndDate(ranges.Selection.endDate);
+  };
+  const resetInput = () => {
+    setSearchInput("");
+  };
+  const handleSearch = () => {};
   return (
     <header className="sticky top-0 z-50 grid grid-cols-3 bg-white shadow-md p-3 md:px-10 ">
       {/* Left div */}
@@ -42,7 +59,37 @@ const Header = () => {
           <UserCircleIcon className="h-6 cursor-pointer" />
         </div>
       </div>
-      {searchInput && <div></div>}
+      {searchInput && (
+        <div className="flex flex-col col-span-3 mx-auto">
+          <DateRangePicker
+            ranges={[selectionRange]}
+            minDate={new Date()}
+            rangeColors={["#FD5B61"]}
+            onChange={handleSelect}
+          />
+          <div className="flex items-center border-b mb-4">
+            <h2 className="text-2xl flex-grow font-semibold">
+              Number of guests
+            </h2>
+            <UsersIcon className="h-8" />
+            <input
+              value={nofGuests}
+              onChange={(e) => setNofGuests(e.target.value)}
+              type="number"
+              min={1}
+              className="w-12 pl-2 text-lg outline-none text-red-400"
+            />
+          </div>
+          <div className="flex">
+            <button onClick={resetInput} className="flex-grow text-gray-500">
+              Cancel
+            </button>
+            <button onClick={handleSearch} className="flex-grow text-red-400">
+              Search
+            </button>
+          </div>
+        </div>
+      )}
     </header>
   );
 };
