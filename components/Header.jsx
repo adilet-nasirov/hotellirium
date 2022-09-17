59117728b92c486b44602f3e7cd1347937c4f603
Adilet Nasirov <adilet.nasirov@yahoo.com>
2022-09-17 08:08:14 -0700

    search input placeholder and router

--- components/Header.jsx
+++ components/Header.jsx
@@ -11,7 +11,8 @@ import {
   UsersIcon,
 } from "@heroicons/react/solid";
 import { useRouter } from "next/router";
-const Header = () => {
+
+const Header = ({placeholder}) => {
   const [searchInput, setSearchInput] = useState("");
   const [startDate, setStartDate] = useState(new Date());
   const [endDate, setEndDate] = useState(new Date());
@@ -58,7 +59,7 @@ const Header = () => {
           onChange={(e) => setSearchInput(e.target.value)}
           className="pl-5 bg-transparent outline-none flex-grow text-gray-600 placeholder-gray-400"
           type="text"
-          placeholder="Start your search"
+          placeholder={placeholder.toUpperCase() || "Start your search"}
         />
         <SearchIcon className="hidden md:inline-flex h-8 bg-red-400 text-white rounded-full p-2 cursor-pointer md: mx-2 " />
       </div>
--- pages/search.jsx
+++ pages/search.jsx
@@ -1,16 +1,26 @@
+import  format  from "date-fns/format";
+import { useRouter } from "next/router";
 import React from "react";
 import Footer from "../components/Footer";
 import Header from "../components/Header";
-
+import moment from "moment/moment";
 function search() {
+  const router = useRouter();
+  const { endDate, location, nofGuests, startDate } = router.query;
+  console.log(moment(startDate).utc().format('DD MMMM YY'));
+//   console.log(format(new Date(startDate), 'dd mm yy'))
+  const formattedStartDate = moment(startDate).utc().format('DD MMMM YY')
+  const formattedEndDate = moment(endDate).utc().format('DD MMMM YY')
+  const range = `- ${formattedStartDate} - ${formattedEndDate} - `;
+
   return (
     <div className="h-screen">
-      <Header />
+      <Header placeholder={`${location} | ${range} | ${nofGuests}`}/>
       <main className="flex">
         <section className="flex-grow pt-12 px-6">
-          <p className="text-xs mt-3">300+ stays for 5 number of guests</p>
+          <p className="text-xs mt-3">300+ stays {range}for {nofGuests} guests</p>
           <h1 className="text-3xl font-semibold mb-6 mt-2">
-            Stays in New-York
+            Stays in {location}
           </h1>
           <div className="hidden lg:inline-flex mb-5 space-x-3 text-gray-800  whitespace-nowrap">
             <p className="button">Cancellation Flexibility</p>
