c35b8ba4a56231f6b182ac70ed0445551c06154c
Adilet Nasirov <adilet.nasirov@yahoo.com>
2022-09-30 07:42:47 -0700

    rapid api

--- pages/search.jsx
+++ pages/search.jsx
@@ -1,17 +1,36 @@
 import  format  from "date-fns/format";
 import { useRouter } from "next/router";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Footer from "../components/Footer";
 import Header from "../components/Header";
 import moment from "moment/moment";
+import axios from "axios";
+
+const options = {
+  method: 'GET',
+  url: 'https://airbnb19.p.rapidapi.com/api/v1/searchProperty',
+  params: {category: 'TAB_8225', totalRecords: '9', currency: 'USD', adults: '1'},
+  headers: {
+    'X-RapidAPI-Key': '9bbfb27703msh3a774eb1fd60944p16626fjsn37ae8f0e05c5',
+    'X-RapidAPI-Host': 'airbnb19.p.rapidapi.com'
+  }
+};
+
 function search() {
   const router = useRouter();
   const { endDate, location, nofGuests, startDate } = router.query;
-  console.log(moment(startDate).utc().format('DD MMMM YY'));
-//   console.log(format(new Date(startDate), 'dd mm yy'))
   const formattedStartDate = moment(startDate).utc().format('DD MMMM YY')
   const formattedEndDate = moment(endDate).utc().format('DD MMMM YY')
   const range = `- ${formattedStartDate} - ${formattedEndDate} - `;
+  const [data, setData] = useState([]);
+  useEffect(()=>{
+    axios.request(options).then(function (response) {
+      console.log(response.data);
+      setData(response.data)
+    }).catch(function (error) {
+      console.error(error);
+    });
+  },[])
 
   return (
     <div className="h-screen">
