d4e81c88c5b3d71ee538a2bda2b1d36928a6796c
Adilet Nasirov <adilet.nasirov@yahoo.com>
2022-10-04 18:34:46 -0700

    progress bar change

--- pages/_app.js
+++ pages/_app.js
@@ -6,13 +6,13 @@ import { Router } from "next/router";
 const progress = new ProgressBar({
   size: 5,
   color: "#FE595E",
-  className: "z-50",
-  delay: 100,
+  className: "bar-of-progress",
+  delay: 80,
 });
 
-Router.events.on("routeChangeStart", progress.start);
-Router.events.on("routeChangeComplete", progress.finish);
-Router.events.on("routeChangeError", progress.finish);
+Router.events.on("routeChangeStart", ()=>progress.start());
+Router.events.on("routeChangeComplete",()=> progress.finish());
+Router.events.on("routeChangeError", ()=>progress.finish());
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
