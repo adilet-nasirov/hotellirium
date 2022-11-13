abc92ae001415e4d05fab9c2a1c82f09b49714d1
Adilet Nasirov <adilet.nasirov@yahoo.com>
2022-11-13 15:39:17 -0800

    filtered data to find clicked item

--- pages/search/[id].jsx
+++ pages/search/[id].jsx
@@ -1,12 +1,23 @@
-import React from "react";
+import React, { useContext } from "react";
 import { useRouter } from "next/router";
+import { DataContext } from "../../lib/DataContext";
+import Header from "../../components/Header";
+import Footer from "../../components/Footer";
+import InfoCard from "../../components/InfoCard";
 const Details = () => {
   const router = useRouter();
   const { id } = router.query;
+  const [state, dispatch] = useContext(DataContext);
+  const { data } = state;
+  const item = data.filter((el) => el.id === id);
+  console.log(item);
   return (
     <div>
+      <Header />
       <h1>Details pagee</h1>
-      <h2>{id}</h2>
+      <InfoCard item={item[0]} />
+
+      <Footer />
     </div>
   );
 };
