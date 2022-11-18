5ae4d4b4db26014dea1fff278969a2999d7bc661
Adilet Nasirov <adilet.nasirov@yahoo.com>
2022-11-17 19:35:24 -0800

    added icons, styled & gallery list

--- components/Header.jsx
+++ components/Header.jsx
@@ -58,74 +58,76 @@ const Header = ({ placeholder }) => {
       });
   };
   return (
-    <header className="sticky top-0 z-50 grid grid-cols-3 bg-white shadow-md p-3 md:px-10">
-      {/* Left div */}
-      <div
-        onClick={() => router.push("/")}
-        className="relative flex items-center h-10 cursor-pointer my-auto"
-      >
-        <Image
-          src="https://links.papareact.com/qd3"
-          layout="fill"
-          objectFit="contain"
-          objectPosition="left"
-        />
-      </div>
-      {/* Middle one */}
-      <div className="flex items-center md:border-2 rounded-full py-2 md:shadow-sm">
-        <input
-          value={searchInput}
-          onChange={(e) => setSearchInput(e.target.value)}
-          className="pl-5 bg-transparent outline-none flex-grow text-gray-600 placeholder-gray-400"
-          type="text"
-          placeholder={placeholder?.toUpperCase() || "Start your search"}
-        />
-        <SearchIcon
-          onClick={handleSearch}
-          className="hidden md:inline-flex h-8 bg-red-400 text-white rounded-full p-2 cursor-pointer md: mx-2 "
-        />
-      </div>
-      {/* Right div */}
-      <div className="flex  space-x-4 items-center justify-end text-gray-500">
-        <p className="cursor-pointer hidden md:inline">Become a host</p>
-        <GlobeAltIcon className="h-6 " />
-        {/* <div className="flex items-center space-x-2 border-2 p-2 rounded-full">
+    <header className="sticky top-0 z-50  bg-white shadow-md p-3 md:px-10">
+      <div className="grid grid-cols-3 max-w-7xl mx-auto">
+        {/* Left div */}
+        <div
+          onClick={() => router.push("/")}
+          className="relative flex items-center h-10 cursor-pointer my-auto"
+        >
+          <Image
+            src="https://links.papareact.com/qd3"
+            layout="fill"
+            objectFit="contain"
+            objectPosition="left"
+          />
+        </div>
+        {/* Middle one */}
+        <div className="flex items-center md:border-2 rounded-full py-2 md:shadow-sm">
+          <input
+            value={searchInput}
+            onChange={(e) => setSearchInput(e.target.value)}
+            className="pl-5 bg-transparent outline-none flex-grow text-gray-600 placeholder-gray-400"
+            type="text"
+            placeholder={placeholder?.toUpperCase() || "Start your search"}
+          />
+          <SearchIcon
+            onClick={handleSearch}
+            className="hidden md:inline-flex h-8 bg-red-400 text-white rounded-full p-2 cursor-pointer md: mx-2 "
+          />
+        </div>
+        {/* Right div */}
+        <div className="flex  space-x-4 items-center justify-end text-gray-500">
+          <p className="cursor-pointer hidden md:inline">Become a host</p>
+          <GlobeAltIcon className="h-6 " />
+          {/* <div className="flex items-center space-x-2 border-2 p-2 rounded-full">
           <MenuIcon className="h-6 cursor-pointer" />
           <UserCircleIcon className="h-6 cursor-pointer" />
         </div> */}
-        <BasicPopover />
-      </div>
-      {searchInput && (
-        <div className="flex flex-col col-span-3 mx-auto">
-          <DateRangePicker
-            ranges={[selectionRange]}
-            minDate={new Date()}
-            rangeColors={["#FD5B61"]}
-            onChange={handleSelect}
-          />
-          <div className="flex items-center border-b mb-4">
-            <h2 className="text-2xl flex-grow font-semibold">
-              Number of guests
-            </h2>
-            <UsersIcon className="h-8" />
-            <input
-              value={nofGuests}
-              onChange={(e) => setNofGuests(e.target.value)}
-              type="number"
-              min={1}
-              className="w-12 pl-2 text-lg outline-none text-red-400"
-            />
-          </div>
-          <div className="flex">
-            <button onClick={resetInput} className="flex-grow text-gray-500">
-              Cancel
-            </button>
-            <button onClick={handleSearch} className="flex-grow text-red-400">
-              Search
-            </button>
-          </div>
+          <BasicPopover />
         </div>
-      )}
+        {searchInput && (
+          <div className="flex flex-col col-span-3 mx-auto">
+            <DateRangePicker
+              ranges={[selectionRange]}
+              minDate={new Date()}
+              rangeColors={["#FD5B61"]}
+              onChange={handleSelect}
+            />
+            <div className="flex items-center border-b mb-4">
+              <h2 className="text-2xl flex-grow font-semibold">
+                Number of guests
+              </h2>
+              <UsersIcon className="h-8" />
+              <input
+                value={nofGuests}
+                onChange={(e) => setNofGuests(e.target.value)}
+                type="number"
+                min={1}
+                className="w-12 pl-2 text-lg outline-none text-red-400"
+              />
+            </div>
+            <div className="flex">
+              <button onClick={resetInput} className="flex-grow text-gray-500">
+                Cancel
+              </button>
+              <button onClick={handleSearch} className="flex-grow text-red-400">
+                Search
+              </button>
+            </div>
+          </div>
+        )}
+      </div>
     </header>
   );
 };
--- pages/search/[id].jsx
+++ pages/search/[id].jsx
@@ -4,7 +4,10 @@ import { DataContext } from "../../lib/DataContext";
 import Header from "../../components/Header";
 import Footer from "../../components/Footer";
 import { FaStar } from "react-icons/fa";
-import { FaBeer } from "react-icons/fa";
+import { FiShare } from "react-icons/fi";
+import { IoMdHeartEmpty } from "react-icons/io";
+import ImageGallery from "react-image-gallery";
+
 const Details = () => {
   const router = useRouter();
   const { id } = router.query;
@@ -13,31 +16,49 @@ const Details = () => {
   console.log(data);
   const filtered = data.filter((el) => el.id === id);
   const item = filtered[0];
-  console.log(filtered);
+  const images = [];
+  for (let image of item.images) {
+    images.push({ original: image, thumbnail: image });
+  }
   return (
     <div>
       <Header />
       {/* information about the hotel */}
-      <section className="2xl:container mx-auto">
-        <h2 className="text-3xl">{item.listingName}</h2>
+      <section className="max-w-7xl mx-auto">
+        <h2 className="text-3xl m-5">{item.listingName}</h2>
         <div className="flex justify-between">
           <div className="flex basis-1/2">
-            <p className="">
-              <FaStar />
-              {item.starRating}
+            {item.starRating && (
+              <h3
+                className="flex items-top mx-3 cursor-pointer
+              "
+              >
+                <FaStar size={20} className="mx-1" />
+                {item.starRating}
+              </h3>
+            )}
+            <p className="underline mx-4 cursor-pointer">
+              {item.reviewsCount} reviews
             </p>
-            <p className="underline">{item.reviewsCount}reviews</p>
-            <p className="underline">{item.publicAddress}</p>
+            <p className="underline mx-5 cursor-pointer">
+              {item.publicAddress}
+            </p>
           </div>
-          <div className="flex flex-grow-1">
-            <p>share</p>
-            <p>save</p>
+          <div className="flex flex-grow-1 ">
+            <p className="flex items-center underline mx-4 cursor-pointer">
+              <FiShare className="mx-2" />
+              share
+            </p>
+            <p className="flex items-center underline mx-4 cursor-pointer">
+              <IoMdHeartEmpty className="mx-2" />
+              save
+            </p>
           </div>
         </div>
-        <h3>
-          {" "}
-          Lets go for a <FaBeer />?{" "}
-        </h3>
+        {/* image gallery */}
+        <div className="my-12">
+          <ImageGallery items={images} />
+        </div>
       </section>
 
       <Footer />
