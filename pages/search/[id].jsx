a0de9364df1f766cf1dce3ecc453c294ada26224
Adilet Nasirov <adilet.nasirov@yahoo.com>
2022-11-17 20:15:41 -0800

    property hightlights, added icons

--- pages/search/[id].jsx
+++ pages/search/[id].jsx
@@ -3,10 +3,17 @@ import { useRouter } from "next/router";
 import { DataContext } from "../../lib/DataContext";
 import Header from "../../components/Header";
 import Footer from "../../components/Footer";
-import { FaStar } from "react-icons/fa";
+import { FaConciergeBell, FaParking, FaStar, FaWifi } from "react-icons/fa";
 import { FiShare } from "react-icons/fi";
-import { IoMdHeartEmpty } from "react-icons/io";
+import { IoMdHeartEmpty, IoMdSnow } from "react-icons/io";
+import { CgGym } from "react-icons/cg";
 import ImageGallery from "react-image-gallery";
+import {
+  MdLocalLaundryService,
+  MdOutlineLocalConvenienceStore,
+  MdOutlineSmokeFree,
+  MdPets,
+} from "react-icons/md";
 
 const Details = () => {
   const router = useRouter();
@@ -30,26 +37,26 @@ const Details = () => {
           <div className="flex basis-1/2">
             {item.starRating && (
               <h3
-                className="flex items-top mx-3 cursor-pointer
+                className="flex items-top mx-3 cursor-pointer text-xl
               "
               >
                 <FaStar size={20} className="mx-1" />
                 {item.starRating}
               </h3>
             )}
-            <p className="underline mx-4 cursor-pointer">
+            <p className="underline mx-4 cursor-pointer text-xl">
               {item.reviewsCount} reviews
             </p>
-            <p className="underline mx-5 cursor-pointer">
+            <p className="underline mx-5 cursor-pointer text-xl">
               {item.publicAddress}
             </p>
           </div>
           <div className="flex flex-grow-1 ">
-            <p className="flex items-center underline mx-4 cursor-pointer">
+            <p className="flex items-center underline mx-4 cursor-pointer text-xl">
               <FiShare className="mx-2" />
               share
             </p>
-            <p className="flex items-center underline mx-4 cursor-pointer">
+            <p className="flex items-center underline mx-4 cursor-pointer text-xl">
               <IoMdHeartEmpty className="mx-2" />
               save
             </p>
@@ -58,6 +65,47 @@ const Details = () => {
         {/* image gallery */}
         <div className="my-12">
           <ImageGallery items={images} />
+        </div>
+        <div className="flex flex-col">
+          <h1 className="text-2xl font-bold m-5"> Property highlights</h1>
+          <div className=" flex-grow-1/2 grid grid-cols-3 m-3">
+            <p className="flex items-center text-xl m-3">
+              <FaWifi className="mx-3" />
+              Free WiFi
+            </p>
+            <p className="flex items-center text-xl m-3">
+              <MdLocalLaundryService className="mx-3" />
+              Laundry facilities
+            </p>
+            <p className="flex items-center text-xl m-3">
+              <IoMdSnow className="mx-3" />
+              Air conditioning
+            </p>
+            <p className="flex items-center text-xl m-3">
+              <MdPets className="mx-3" />
+              Pet friendly
+            </p>
+            <p className="flex items-center text-xl m-3">
+              <MdOutlineSmokeFree className="mx-3" />
+              Non-smoking
+            </p>
+            <p className="flex items-center text-xl m-3">
+              <MdOutlineLocalConvenienceStore className="mx-3" />
+              24/7 front desk
+            </p>
+            <p className="flex items-center text-xl m-3">
+              <FaParking className="mx-3" />
+              Free parking
+            </p>
+            <p className="flex items-center text-xl m-3">
+              <CgGym className="mx-3" />
+              GYM
+            </p>
+            <p className="flex items-center text-xl m-3">
+              <FaConciergeBell className="mx-3" />
+              Concierge services
+            </p>
+          </div>
         </div>
       </section>
 
