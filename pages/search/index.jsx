236e2d4c7a658f77244efb125226ddff84a351bb
Adilet Nasirov <adilet.nasirov@yahoo.com>
2022-11-30 17:15:02 -0800

    data length

--- pages/search/index.jsx
+++ pages/search/index.jsx
@@ -80,7 +80,7 @@ function Search() {
             <p className="button">More filters...</p>
           </div>
           <div>
-            {data !== null ? (
+            {data.length ? (
               data.map((item) => (
                 <InfoCard item={item} days={days} key={item.id} />
               ))
